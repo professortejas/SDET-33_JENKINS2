78032dd4178b20644a497bf1feda840eef925422
Dell <Dell@Dell-PC>
2022-04-14 13:04:09 +0530

    commit 1

---
+++ src/test/java/jenkinssample/Class1Test.java
@@ -0,0 +1,8 @@
+package jenkinssample;
+
+public class Class1Test {
+
+	public void m1Test() {
+		System.out.println("m1 test from class 1");
+	}
+}
---
+++ src/test/java/jenkinssample/Class2Test.java
@@ -0,0 +1,8 @@
+package jenkinssample;
+
+public class Class2Test {
+
+	public void m1Test() {
+		System.out.println("m1 test from class 2");
+	}
+}
